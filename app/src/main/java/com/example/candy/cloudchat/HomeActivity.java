17057529324c23745e76ec50414b09b0a3c04e05
sydnieec <sydnie.chau@gmail.com>
2018-08-30 15:37:01 -0400

    post request for user login

--- app/src/main/java/com/example/candy/cloudchat/HomeActivity.java
+++ app/src/main/java/com/example/candy/cloudchat/HomeActivity.java
@@ -8,6 +8,8 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
 import org.json.*;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -16,12 +18,20 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import java.util.Map;
+import java.util.HashMap;
+
 
 public class HomeActivity extends AppCompatActivity {
     private static final String TAG =HomeActivity.class.getName();
     private RequestQueue mRequestQueue;
     private StringRequest stringRequest;
     private String url = "http://www.mocky.io/v2/5b874a32340000db018b58b8";
+    private String login_url="http://ec2-13-59-209-87.us-east-2.compute.amazonaws.com:4000/login";
+    private StringRequest MyStringRequest;
+    private RequestQueue MyRequestQueue;
+    private boolean valid_user;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,13 +44,54 @@ public class HomeActivity extends AppCompatActivity {
 
                EditText a= (EditText) findViewById(R.id.TFusername);
                 String str= a.getText().toString();
-                sendRequestAndPrintResponse();
-                Intent i= new Intent (HomeActivity.this, Display.class);
-                i.putExtra("Username",str );
-                startActivity(i);
+                sendPost();
+
+                if (valid_user=false) {
+                Toast errorToast = Toast.makeText(HomeActivity.this, "Invalid username or password", Toast.LENGTH_SHORT);
+                errorToast.show();
+
+                    }else {
+                    //  sendRequestAndPrintResponse();
+                    Intent i = new Intent(HomeActivity.this, Display.class);
+                    i.putExtra("Username", str);
+                    startActivity(i);
+                }
             }});
     }
+    private void sendPost(){
+        MyRequestQueue=Volley.newRequestQueue(this);
+         MyStringRequest = new StringRequest(Request.Method.POST, login_url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject obj = new JSONObject(response);
+                    String valid = obj.getJSONObject("status").getString("error");
+                    Log.i(TAG, "Response: " + valid);
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
 
+
+            }
+        }, new Response.ErrorListener() { //Create an error listener to handle errors appropriately.
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.i(TAG, "Error" + error.toString());
+            }
+        }) {
+            protected Map<String, String> getParams() {
+                Map<String, String> MyData = new HashMap<String, String>();
+                MyData.put("Username:", "bob");
+                MyData.put ("Password:", "je" );//Add the data you'd like to send to the server.
+                return MyData;
+
+            }
+
+        };
+
+
+            MyRequestQueue.add(MyStringRequest);
+    }
     private void sendRequestAndPrintResponse() {
         mRequestQueue = Volley.newRequestQueue(this);
         stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
@@ -48,9 +99,8 @@ public class HomeActivity extends AppCompatActivity {
             @Override
             public void onResponse(String response) {
                 try {
-
-                    JSONObject obj = new JSONObject(response);
-                    String pageName = obj.getJSONObject("pageInfo").getString("pageName");
+                   JSONObject obj = new JSONObject(response);
+                   String pageName = obj.getJSONObject("pageInfo").getString("pageName");
                     JSONArray arr = obj.getJSONArray("posts");
                         String post_id = arr.getJSONObject(0).getString("post_id");
                     Log.i(TAG, "Response: " + pageName+" "+ post_id );
